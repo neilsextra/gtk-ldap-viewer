9693fcf0c38a36079dab0fe192f70f48a5e912b7
thesourorange <thesourorange@outlook.com>
2025-07-10 22:08:32 +1000

    Fixed up connector

--- src/main/java/org/tso/ldap/DirectoryConnection.java
+++ src/main/java/org/tso/ldap/DirectoryConnection.java
@@ -19,11 +19,11 @@ public class DirectoryConnection implements Runnable {
 
         String[] parts = url.split("/|:|@");
 
-        if (parts.length == 7) {
+        if (parts.length == 6) {
             this.properties.put("protocol", parts[0]);
             this.properties.put("username", parts[3]);
-            this.properties.put("host", parts[5]);
-            this.properties.put("port", parts[6]);
+            this.properties.put("host", parts[4]);
+            this.properties.put("port", parts[5]);
         } else {
             throw new Exception("Invalid URL");
         }
